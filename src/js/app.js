c0cbb52d55a6cf255319294bdf668db4a19a977a
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-05-25 13:28:24 +0000

    city and country manipulation

--- src/js/app.js
+++ src/js/app.js
@@ -35,8 +35,8 @@ function render(variables = {}) {
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
