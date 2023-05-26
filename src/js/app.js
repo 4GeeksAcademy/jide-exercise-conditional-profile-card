9267cc8dfd0f985672c5c7b83292fff24cc4c494
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-05-26 20:30:28 +0000

    add social media links

--- src/js/app.js
+++ src/js/app.js
@@ -37,10 +37,10 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a>${variables.twitter}</li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a>${variables.github}</li>
+            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a>${variables.linkedin}</li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a>${variables.instagram}</li>
           </ul>
         </div>
     `;
@@ -60,10 +60,10 @@ window.onload = function() {
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
-    github: "alesanchezr",
-    linkedin: null,
-    instagram: null,
+    twitter: "",
+    github: "",
+    linkedin: "",
+    instagram: "",
     name: "Your name",
     lastname: "Your lastname",
     role: "Current role",
